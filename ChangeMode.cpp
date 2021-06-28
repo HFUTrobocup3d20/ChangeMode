46c5cc027dbbe0ce684c611dd68051cd6b2e89b7
温柔的风狮子 <42141643+twofive2003@users.noreply.github.com>
2021-06-28 22:39:15 +0800

    ChangeMode
    一个看起来能用的镜像)

---
+++ ChangeMode.cpp
@@ -0,0 +1,20 @@
+void WorldModel::ChangePlayMode()
+{
+    int PlayMode = getPlayMode();
+    if (getSide() == SIDE_RIGHT)
+    {
+        setSide(SIDE_LEFT);
+        if (PlayMode == PM_KICK_OFF_RIGHT || PlayMode == PM_KICK_IN_RIGHT || PlayMode == PM_GOAL_RIGHT || PlayMode == PM_CORNER_KICK_RIGHT || PlayMode == PM_GOAL_KICK_RIGHT || PlayMode == PM_OFFSIDE_RIGHT || PlayMode == PM_FREE_KICK_RIGHT || PlayMode == PM_DIRECT_FREE_KICK_RIGHT || PlayMode == PM_PASS_RIGHT)
+        {
+            setPlayMode(PlayMode - 1);
+        }
+    }
+    /*else if (getSide() == SIDE_LEFT)
+    {
+        setSide(SIDE_RIGHT);
+        if (PlayMode == PM_KICK_OFF_LEFT || PlayMode == PM_KICK_IN_LEFT || PlayMode == PM_GOAL_LEFT || PlayMode == PM_CORNER_KICK_LEFT || PlayMode == PM_GOAL_KICK_LEFT || PlayMode == PM_OFFSIDE_LEFT || PlayMode == PM_FREE_KICK_LEFT || PlayMode == PM_DIRECT_FREE_KICK_LEFT || PlayMode == PM_PASS_LEFT)
+        {
+            setPlayMode(PlayMode + 1);
+        }
+    }*/
+}
