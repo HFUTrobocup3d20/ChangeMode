517f2ad23715f0a3e04173777cb445b4f32dc67b
温柔的风狮子 <42141643+twofive2003@users.noreply.github.com>
2021-07-08 00:21:32 +0800

    Add files via upload

--- worldmodel.cc
+++ worldmodel.cc
@@ -97,27 +97,6 @@ WorldModel::~WorldModel()
     delete opponentKalmanFilters;
 }
 
-void WorldModel::ChangePlayMode()
-{
-    int PlayMode = getPlayMode();
-    if (getSide() == SIDE_RIGHT)
-    {
-        setSide(SIDE_LEFT);
-        if (PlayMode == PM_KICK_OFF_RIGHT || PlayMode == PM_KICK_IN_RIGHT || PlayMode == PM_GOAL_RIGHT || PlayMode == PM_CORNER_KICK_RIGHT || PlayMode == PM_GOAL_KICK_RIGHT || PlayMode == PM_OFFSIDE_RIGHT || PlayMode == PM_FREE_KICK_RIGHT || PlayMode == PM_DIRECT_FREE_KICK_RIGHT || PlayMode == PM_PASS_RIGHT)
-        {
-            setPlayMode(PlayMode - 1);
-        }
-    }
-    /*else if (getSide() == SIDE_LEFT)
-    {
-        setSide(SIDE_RIGHT);
-        if (PlayMode == PM_KICK_OFF_LEFT || PlayMode == PM_KICK_IN_LEFT || PlayMode == PM_GOAL_LEFT || PlayMode == PM_CORNER_KICK_LEFT || PlayMode == PM_GOAL_KICK_LEFT || PlayMode == PM_OFFSIDE_LEFT || PlayMode == PM_FREE_KICK_LEFT || PlayMode == PM_DIRECT_FREE_KICK_LEFT || PlayMode == PM_PASS_LEFT)
-        {
-            setPlayMode(PlayMode + 1);
-        }
-    }*/
-}
-
 void WorldModel::display()
 {
 
@@ -143,6 +122,49 @@ void WorldModel::display()
     }
 
     cout << "*********************************************************\n";
+}
+
+void WorldModel::ChangePlayMode()
+{
+    int PlayMode = getPlayMode();
+    if (getSide() == SIDE_RIGHT)
+    {
+        //setSide(SIDE_LEFT);
+        if (PlayMode == PM_KICK_OFF_RIGHT || PlayMode == PM_KICK_IN_RIGHT || PlayMode == PM_GOAL_RIGHT || PlayMode == PM_CORNER_KICK_RIGHT ||
+            PlayMode == PM_GOAL_KICK_RIGHT || PlayMode == PM_OFFSIDE_RIGHT || PlayMode == PM_FREE_KICK_RIGHT || PlayMode == PM_DIRECT_FREE_KICK_RIGHT ||
+            PlayMode == PM_PASS_RIGHT)
+        {
+            setPlayMode(PlayMode - 1);
+        }
+        if (PlayMode == PM_KICK_OFF_LEFT || PlayMode == PM_KICK_IN_LEFT || PlayMode == PM_GOAL_LEFT || PlayMode == PM_CORNER_KICK_LEFT ||
+            PlayMode == PM_GOAL_KICK_LEFT || PlayMode == PM_OFFSIDE_LEFT || PlayMode == PM_FREE_KICK_LEFT || PlayMode == PM_DIRECT_FREE_KICK_LEFT ||
+            PlayMode == PM_PASS_LEFT)
+        {
+            setPlayMode(PlayMode + 1);
+        }
+    }
+}
+
+double WorldModel::getChangeModeTime()
+{
+    static int OldPlayMode;
+    static double OldTime;
+    int NowMode = getPlayMode();
+    double NowTime = getGameTime();
+    if (NowTime == 0.0)
+    {
+        OldPlayMode = NowMode;
+        OldTime = NowTime;
+    }
+    else
+    {
+        if (OldPlayMode != NowMode)
+        {
+            OldPlayMode = NowMode;
+            OldTime = NowTime;
+        }
+    }
+    return NowTime - OldTime;
 }
 
 void WorldModel::updateGoalPostsAndFlags()
